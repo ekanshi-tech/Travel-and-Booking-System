31704756c0e1bb52ef4e583cba83b78abbc25b58
Ekanshi <ekanshis79@gmail.com>
2023-09-01 15:59:35 +0530

    First Commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv'
 import userRoutes from "./routes/users.js"
 
+
 const app = express();
+dotenv.config();
 app.use(express.json({linit:"30mb", extended:true}))
 app.use(express.urlencoded({linit:"30mb", extended:true}))
 app.use(cors());
@@ -17,7 +20,7 @@ app.use('/user',userRoutes);
 const PORT=process.env.PORT || 8800
 
 
-const CONNECTION_URL="mongodb+srv://ekanshi:ekanshi@tour-and-travel.evhyolj.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL=process.env.DATABASE_URL
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=> app.listen(PORT,()=>{console.log(`server running on port ${PORT}`)}))
 .catch((err) => console.log(err.message))
